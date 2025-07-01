03823e2e17a40bb2ee832abe518c419c985db92f
Vk-246 <vinayakkanhegaonkar5383@gmail.com>
2025-07-01 18:06:14 +0530

    create sign up API route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs"; //importing bcryptjs for password hashing
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body; //passwd can't be saved as it is, it should be hashed before saving. hence destructuring the request body
+    const hashedPassword = bcryptjs.hashSync(password, 10); //hashing the password with a salt rounds of 10
+    const newUser = new User({username, email, password: hashedPassword}); //creating a new user instance with the provided username, email, and password
+      try {
+        await newUser.save(); //saving the new user to the database
+        res.status(201).json({ message: "User created successfully!" }); //responding with a success message and a 201 status code
+        } catch (error) {
+          res.status(500).json(error.message); //if there's an error, responding with a 500 status code and the error message
+      }
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -15,9 +16,13 @@ mongoose
 
 const app = express();
 
-app.use('/api/user', userRouter);
+app.use(express.json());//allows us to parse JSON bodies, by default, Express does not parse JSON bodies, so we need to use this middleware to handle JSON requests.
 
 app.listen(3000, () => {
   console.log('Server is running on port 4000!');
 });
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
 
