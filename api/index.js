4a8cb5340dafeec7459244efedb58590df681e8c
Vk-246 <vinayakkanhegaonkar5383@gmail.com>
2025-07-12 16:46:25 +0530

    complete home page

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
 
 dotenv.config();
@@ -17,8 +18,7 @@ mongoose
     console.log(err);
   });
 
-const app = express();
-
+const app = express();//create instance of Express application,sets up server and define routes and middleware.
 
 app.use(express.json());//allows us to parse JSON bodies, by default, Express does not parse JSON bodies, so we need to use this middleware to handle JSON requests.
 app.use(cookieParser());//allows us to parse cookies from incoming requests, enabling us to access cookies in the request object.
@@ -40,3 +40,9 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
+//middlewares,mongoose, env, dotenv, cookie-parser, express, and routes are imported.
+//mongoose connect to MongoDB, dotenv is used to load environment variables, cookie-parser is used to parse cookies, and express is used to create the server.
+//The server listens on port 3000, and the routes for user, auth, and listing are defined.
+//An error handling middleware is also defined to catch any errors that occur during request processing and return  a JSON response with the error details.   
+//app.use() is used to define middleware functions that will be executed in the order they are defined, allowing us to handle requests and responses effectively.
